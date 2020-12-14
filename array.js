43064d0f29a3ec1ccc2e62ed0b591ff2015a8dc2
Bunks <busk_1@yahoo.com>
2020-12-14 18:00:00 -0500

    added a timer and another click event for quiz buttons

--- array.js
+++ array.js
@@ -1,43 +1,28 @@
 // array of ofjects containing questions
-questions = [];
 
-quest1 = {
+var quest1 = {
   question: "Commonly used data types DO NOT include:",
-  answer1: "strings",
-  answer2: "booleans",
-  answer3: "assets",
-  answer4: "numbers",
-  correct: "answer3"
+  answer: ["strings", "booleans", "assets", "numbers"],
+  correct: 2
 };
-quest2 = {
+var quest2 = {
   question: "The condition in an if / else statement is enclosed within ____.",
-  answer1: "quotes",
-  answer2: "curly brackets",
-  answer3: "parentheses",
-  answer4: "square brackets",
-  correct: "answer2"
+  answer: ["quotes", "curly brackets", "parentheses", "square brackets"],
+  correct: 1
 };
-quest3 = {
+var quest3 = {
   question: "Arrays in JavaScript can be used to store ____",
-  answer1: "numbers and strings",
-  answer2: "other arrays",
-  answer3: "booleans",
-  answer4: "ALL of the above",
-  correct: "answer4"
+  answer: ["numbers and strings", "other arrays", "booleans", "ALL of the above"],
+  correct: 3
 };
-quest4 = {
+var quest4 = {
   question: "String values must be enclosed within _____ when being assigned to variables.",
-  answer1: "commas",
-  answer2: "curly brackets",
-  answer3: "quotes",
-  answer4: "parentheses",
-  correct: "answer3"
+  answer: ["commas", "curly brackets", "quotes", "parentheses"],
+  correct: 2
 };
-quest5 = {
+var quest5 = {
   question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-  answer1: "JavaScript",
-  answer2: "terminal / bash",
-  answer3: "for loops",
-  answer4: "console log",
-  correct: "answer4"
+  answer: ["JavaScript", "terminal / bash", "for loops", "console log"],
+  correct: 3
 };
+quizQuestions = [quest1,quest2,quest3,quest4,quest5];
